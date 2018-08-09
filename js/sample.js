4cb9313398cc9dcf7a8d8dea88bdc823d4a5f7d5
ajinorisenpai <thunderlove1111@gmail.com>
2018-08-10 00:53:04 +0900

    インタラクティブ化 v0.1

--- js/sample.js
+++ js/sample.js
@@ -52,13 +52,16 @@
 		// ヒートマップを表示
 		heatMap.enter()
 			.append("rect")  // rect要素を追加
-			.attr("class", "block")	// CSSクラスを追加
-            .attr("class",function(d, i) { // X座標を設定
-                return "col" + (i % dataWidth);
-            })
+			// .attr("class", "block")	// CSSクラスを追加
+            // .attr("class",function(d, i) { // X座標を設定
+             //    return "block x" + (i % dataWidth) + " y" + parseInt(i / dataWidth);
+            // })
+            // .attr("class",function(d, i) { // X座標を設定
+            //     return "y" + (i / dataWidth);
+            // })
 			.attr("x", function(d, i) { // X座標を設定
 				return (i % dataWidth) * blockSize;
-			}) 
+			})
 			.attr("y", function(d, i) { // Y座標を設定
 				return Math.floor(i/dataWidth)*blockSize;
 			})
@@ -74,20 +77,27 @@
 			    // if(d > 10) return color(1);
 				return color(Math.log10(d/5));
 			})
-            .attr("stroke-width",1)
-            .on("mouseover", function(d,i){
-                // console.log("aaa");
-                 document.getElementById("headfix").innerHTML = d;
-                 d3.selectAll(".col"+(i % dataWidth)).attr("style","fill:rgb(0,255,0)")
-            })
-            .on("mouseout",function(d,i){
-                d3.selectAll(".col"+(i % dataWidth)).style("fill",function(d, i){	// 色を表示
-                    if(d == 0) return "#9599ff";
-                    if(d==1) return "#e1e9ff"
-                    // if(d > 10) return color(1);
-                    return color(Math.log10(d/5));
-                });
-            })
+            .attr("stroke-width",0)
+            // .on("mouseover", function(d,i){
+            //     // console.log("aaa");
+            //      document.getElementById("headfix").innerHTML = d;
+            //      d3.selectAll(".x"+(i % dataWidth)).attr("style","fill:rgb(0,255,0)");
+            //      d3.selectAll(".y"+parseInt(i / dataWidth)).attr("style","fill:rgb(0,255,0)");
+            // })
+            // .on("mouseout",function(d,i){
+            //     d3.selectAll(".x"+(i % dataWidth)).style("fill",function(d, i){	// 色を表示
+            //         if(d == 0) return "#9599ff";
+            //         if(d==1) return "#e1e9ff"
+            //         // if(d > 10) return color(1);
+            //         return color(Math.log10(d/5));
+            //     });
+            //     d3.selectAll(".y"+parseInt(i / dataWidth)).style("fill",function(d, i){	// 色を表示
+            //         if(d == 0) return "#9599ff";
+            //         if(d==1) return "#e1e9ff"
+            //         // if(d > 10) return color(1);
+            //         return color(Math.log10(d/5));
+            //     });
+            // })
 
         // マップ内に文字を追加
         heatMap.enter()
@@ -104,6 +114,84 @@
             .attr("dy", "0.35em")	// 表示位置を調整
             .text(function(d, i) {  // 文字を表示する
                 return d;	// 領域内に表示する文字を返す
+            })
+
+        heatMap.enter()
+            .append("rect")  // rect要素を追加
+            .call(function(elements){
+                elements.each(function(d,i){
+                    rectlists.push()
+                })
+            })
+            // .attr("class", "block")	// CSSクラスを追加
+            .attr("class",function(d, i) { // X座標を設定
+                return "block x" + (i % dataWidth) + " y" + parseInt(i / dataWidth);
+            })
+            // .attr("class",function(d, i) { // X座標を設定
+            //     return "y" + (i / dataWidth);
+            // })
+            .attr("x", function(d, i) { // X座標を設定
+                return (i % dataWidth) * blockSize;
+            })
+            .attr("y", function(d, i) { // Y座標を設定
+                return Math.floor(i/dataWidth)*blockSize;
+            })
+            .attr("width", function(d, i) {	// 横幅を設定
+                return blockSize;
+            })
+            .attr("height", function(d, i) {	// 縦幅を設定
+                return blockSize;
+            })
+            .style("fill", function(d, i){	// 色を表示
+                	    if(d == 0) return "#9599ff";
+                	    if(d==1) return "#e1e9ff"
+                	    // if(d > 10) return color(1);
+                		return color(Math.log10(d/5));
+                	})
+            .style("fill-opacity","0.0")
+            .attr("stroke-width",0)
+            .on("mouseover", function(d,i){
+                // console.log("aaa");
+                document.getElementById("headfix").innerHTML = d;
+                let cx = i % dataWidth;
+                let cy = parseInt(i / dataWidth);
+
+                for(let j=0;j<cx;j++){
+                    d3.selectAll(".x"+j).filter(".y"+cy).attr("style","fill:rgb(0,255,0)")
+                        .style("fill-opacity","0.3");
+                    d3.selectAll(".x"+j).filter(".y"+parseInt(cy-cx+j)).attr("style","fill:rgb(0,255,0)")
+                        .style("fill-opacity","0.3");
+
+                }
+                for(let j=0;j<cy;j++){
+                    d3.selectAll(".y"+j).filter(".x"+cx).attr("style","fill:rgb(0,255,0)")
+                        .style("fill-opacity","0.3");
+                }
+                // d3.selectAll(".x"+(i % dataWidth)).attr("style","fill:rgb(0,255,0)")
+                //     .style("fill-opacity","0.3");
+                // d3.selectAll(".y"+cy).attr("style","fill:rgb(0,255,0)").style("fill-opacity","0.3");
+            })
+            .on("mouseout",function(d,i){
+                // d3.selectAll(".x"+(i % dataWidth)).style("fill-opacity","0.0")
+                // d3.selectAll(".y"+cy).style("fill",function(d, i){	// 色を表示
+                //     if(d == 0) return "#9599ff";
+                //     if(d==1) return "#e1e9ff"
+                //     // if(d > 10) return color(1);
+                //     return color(Math.log10(d/5));
+                // });
+                let cx = i % dataWidth;
+                let cy = parseInt(i / dataWidth);
+                for(let j=0;j<cx;j++){
+                    d3.selectAll(".x"+j).filter(".y"+cy).attr("style","fill:rgb(0,255,0)")
+                        .style("fill-opacity","0.0");
+                    d3.selectAll(".x"+j).filter(".y"+parseInt(cy-cx+j)).attr("style","fill:rgb(0,255,0)")
+                        .style("fill-opacity","0.0");
+
+                }
+                for(let j=0;j<cy;j++){
+                    d3.selectAll(".y"+j).filter(".x"+cx).attr("style","fill:rgb(0,255,0)")
+                        .style("fill-opacity","0.0");
+                }
             })
 
     }
